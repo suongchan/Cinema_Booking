cdd6c41aa83abacbe667486e802063adcb408ca2
suongchan <minhsuong19102002@gmail.com>
2023-09-25 15:19:14 +0700

    project Cinema

---
+++ src/main/java/com/example/cinema_booking/CinemaBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinema_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBookingApplication.class, args);
+    }
+
+}
